79c1d534655f67090e11157e9e44480d5f8799ac
Vansh Mittal <mittalvansh504@gmail.com>
2026-05-07 01:28:54 +0530

    Link With Fontend

--- src/main/java/com/system/leaveManagement/EmployeeDeashboard/Entity/Controller/EmployeeController.java
+++ src/main/java/com/system/leaveManagement/EmployeeDeashboard/Entity/Controller/EmployeeController.java
@@ -6,13 +6,11 @@ import com.system.leaveManagement.EmployeeDeashboard.Entity.Request.EmployeeDto;
 import com.system.leaveManagement.EmployeeDeashboard.Entity.Request.RequestForEmployee;
 import com.system.leaveManagement.EmployeeDeashboard.Entity.Request.RequestForLogin;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EmployeeController {
 
     @Autowired
